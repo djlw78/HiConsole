efc0ff4a8e43d884ffbfdc037f3d60fdf958e7e1
djlw78 <djlw78@gmail.com>
2020-03-24 00:36:44 -0500

    Putting the base in
    Updated Code Finally

---
+++ HIConsole/HIConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Commander;
+namespace HIConsole
+{
+    class Program
+    {
+        public static CommandProcessor processor;
+        static void Main(string[] args) => new Program().Run(args);
+
+        public Program()
+        {
+            Console.WriteLine("HiConsole [Version 1.0.0.0b");
+            Console.WriteLine("(c) 2020 Indie Developer. All rights reserved");
+            processor = new CommandProcessor();
+            processor.DefaultFGColor = ConsoleColor.White;
+            processor.DefaultBGColor = ConsoleColor.Black;
+            processor.ApplicationName = new AppName("HiConsole", new CommandVersion(1, 0, 0, 0, "b"));
+        }
+
+        public void Run(string[] args)
+        {
+            processor.Process(true);
+        }
+    }
+}
